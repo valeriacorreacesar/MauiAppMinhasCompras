f0285a8270daeada394447eb5ab53ea2b6ef5476
valeriacorreacesar <valeriacesar@live.com>
2025-08-24 22:44:44 -0300

    Quase o final da aula 3

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -5,11 +5,11 @@ namespace MauiAppMinhasCompras.Helpers
 {
     public class SQLiteDatabaseHelper
     {
-        readonly SQLiteConnection _conn;
+        readonly SQLiteAsyncConnection _conn;
         public SQLiteDatabaseHelper(string path)
         {
             _conn = new SQLiteAsyncConnection(path);
-            _conn = CreateTableAsync<Produto>().Wait();
+            _conn.CreateTableAsync<Produto>().Wait();
 
         }
 
