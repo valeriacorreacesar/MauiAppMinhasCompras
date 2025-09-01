d38af4cd0d39faafd445f3670949b19b4d89c467
valeriacorreacesar <valeriacesar@live.com>
2025-08-31 21:32:25 -0300

    Agenda 4 - final

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,26 @@
+﻿
+using System.Collections.ObjectModel;
+using MauiAppMinhasCompras.Models;  // ← LINHA NOVA AQUI
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
+
+    protected async override void OnAppearing()
+    {
+		List<Produto> tmp = await App.DbGetAll();
+
+		tmp.ForEach(i => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
